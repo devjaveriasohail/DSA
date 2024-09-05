a01e07f673e480c403e90cbc246cb3122beccc1b
devjaveriasohail <javeriasohail996@gmail.com>
2024-09-05 13:34:23 +0500

    day2 Input

---
+++ C++basic/4.input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Input 
+    //to take input from user we use cin with this  >> symbol and store it in variable
+     int age;  // a variable age is declare without any value
+      cout<< "Enter your age ";  // added this line so the user known what to enter
+    cin>>age;// now we take input from user
+
+   // here we print the value that user enter
+    cout << "your age is "<<age;
+    return 0;
+}
