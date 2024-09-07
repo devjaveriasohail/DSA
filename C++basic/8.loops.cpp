7371ec5241399c57d6341e33309177b50bcd5e9f
devjaveriasohail <javeriasohail996@gmail.com>
2024-09-07 14:32:23 +0500

    Day 3 loops

---
+++ C++basic/8.loops.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+/*
+    //While loop
+    int count = 1;
+    while (count <=10){
+        cout<<count<<"  ";
+        count ++;
+    }
+
+    // practice question
+    //print the number until n value (here n value taken from user)
+    int n;
+    int i = 0;
+    cout<<"Enter the number";
+    cin>>n; 
+    while (i <=n){
+        cout<<i<<"  ";
+        i ++;
+    }
+*/
+//doing the same task with for loop uncomment or comment the respective part/portion
+// for loop  
+   for(int i=1;i<=10;i++){
+    cout<<i<<" \n";
+   }
+
+    // practice question
+    //print the number until n value (here n value taken from user)
+    int n;
+    cout<<"Enter the number ";
+    cin>>n;
+    for (int j=0;j<=n;j++){
+        cout<<j<<" "; 
+    }
+
+// increasing count value by 2
+    int num;
+    cout<<"Enter the number ";
+    cin>>num;
+    for (int k=0;k<=n;k=k+2){
+        cout<<k<<" "; 
+    }
+   
+    return 0;
+}
