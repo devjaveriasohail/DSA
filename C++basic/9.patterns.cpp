3e5dead8957461b36890bd556f7175d7ad722675
devjaveriasohail <javeriasohail996@gmail.com>
2024-09-09 14:29:25 +0500

    Day 4 Patterns

--- C++basic/9.patterns.cpp
+++ C++basic/9.patterns.cpp
@@ -46,5 +46,13 @@ int main() {
         cout<<endl; 
     }
 
+    //triangle Patterns
+    for(int i=1; i<=n; i++){      
+        for(int j=1; j<=i; j++){ 
+        cout<<"*";
+        }  
+        cout<<endl; 
+    }
+
     return 0;
 }
