a527252fd0c566c8219badd3946b857f1553f9d9
devjaveriasohail <javeriasohail996@gmail.com>
2024-09-11 11:31:06 +0500

    Day 4 Patterns

--- C++basic/9.patterns.cpp
+++ C++basic/9.patterns.cpp
@@ -194,5 +194,40 @@ int main() {
         cout<<endl;
        }
 
+       //hollow diamond pattern
+
+       //top part
+       for (int i=0;i<n;i++){
+        //spaces
+        for(int j=0;j<n-1-i;j++){
+            cout<<" ";
+        }
+        cout<<"*"; 
+        //spaces2
+        if (i!=0){
+            for(int j=0;j<2*i-1; j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+        cout<<endl;
+        }
+        //bottom part
+        for(int i=0; i<n-1; i++){
+            //spaces
+             for(int j=0;j<i+1;j++){
+            cout<<" ";
+        }
+           cout<<"*"; 
+        //spaces
+        if (i!=n-2){
+            for(int j=0;j<2*(n-i)-5; j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+        cout<<endl;
+        }
+
     return 0;
 }
