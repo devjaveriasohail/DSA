1b0a2d54b7893412310b012d0edf9c66c4b36729
devjaveriasohail <javeriasohail996@gmail.com>
2024-09-16 14:20:07 +0500

    Day 6 binaryNumberSystem

--- C++basic/11.binaryNumber.cpp
+++ C++basic/11.binaryNumber.cpp
@@ -15,10 +15,29 @@ int deciToBinary(int deciNum){
     return ans;
 }
 
+int binaToDecimal(int binaNum){
+    int ans=0;
+    int pow=1;
+    for (int i=1; binaNum>0; i++){
+     int remain;
+     remain= binaNum % 10;
+      ans += remain*pow;
+     binaNum /=10 ;
+      pow *= 2;
+    }
+    return ans;
+
+}
+
 int main() {
     int deciNum;
     cout << "Enter a decimal number to convert to binary number ";
     cin>> deciNum; 
-    cout<<"The binary number of "<<deciNum<<" is "<<deciToBinary(deciNum);
+    cout<<"The binary number of "<<deciNum<<" is "<<deciToBinary(deciNum)<<"\n";
+
+       int binaNum;
+    cout << "Enter a binary number to convert to decimal number ";
+    cin>> binaNum; 
+    cout<<"The decimal number of "<<binaNum<<" is "<<binaToDecimal(binaNum);
     return 0;
 }
