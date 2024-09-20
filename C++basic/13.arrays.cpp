440d38e59669cc58ac190a4b4b697ad8bd4d4f2f
devjaveriasohail <javeriasohail996@gmail.com>
2024-09-20 11:06:06 +0500

    Day 8 arrays

--- C++basic/13.arrays.cpp
+++ C++basic/13.arrays.cpp
@@ -3,58 +3,65 @@ using namespace std;
 
 int main() {
     // arrays
-    int student[6]; //declartion
-    int marks[5]={100,89,87,67,45}; //declartion +initialization
+    int marks[10]; //declartion
+    int number[5]={100,89,87,67,45}; //declartion +initialization
     double price[]={67.90,78,56.0};
 
     //find size of price array
     int size=sizeof(price)/sizeof(double);
     cout<<"size of array price is "<<size<<endl;
+     
+     // print the price 
+    cout<<"The prices are ";
+    for(int i=0; i<size; i++){
+        cout<<price[i]<<" ";
+    }
+     cout<<endl;
 
     //inputing values for student array
-    cout<<"enter the marks of student ";
-    for(int i=0; i<6; i++){
-        cin>>student[i];
+    cout<<"Enter the marks of student ";
+    for(int i=0; i<10; i++){
+        cin>>marks[i];
     }
 
-    // print the marks of students
-    cout<<"The marks of students are ";
-    for(int i=0; i<6; i++){
-        cout<<student[i]<<" ";
-    }
-    cout<<endl;
-
-   // find the smallest and largest value in array
-
-   // input the values from user
-   int value[10];
-   int sizeValue=10; // fixing the size of value array and assigninig it to variable
-
-    cout<<"Enter the values to find largest and smaller in them ";
-    for(int i=0; i<sizeValue; i++){
-        cin>>value[i];
-    }
+   // find the smallest and largest marks in array(marks)
 
     // finding the largest and smallest value
     int largest=INT_MIN;
     int smallest=INT_MAX;
-     for(int i=0; i<sizeValue; i++){
+     for(int i=0; i<10; i++){
+       // finding using for if comparsion
         // if(value[i]>largest){
         //    largest=value[i];
-        // }
-        largest=max(value[i],largest);
-
+        // }  
         // if(value[i]<smallest){
         //     smallest=value[i];
         // }
-
-       smallest= min(value[i],smallest);
+         
+    //    finding using built in function max and min;
+        largest=max(marks[i],largest);
+       smallest= min(marks[i],smallest);
     }
     //printing the answer
      cout<<"\nThe largest value is "<<largest;
      cout<<"\nThe smallest value is "<<smallest;
+     cout<<endl;
      
-
+    // linear search
+    int find=100;
+    int index=-1;
+    for(int i=0; i<10; i++){
+      if (marks[i]==find){
+        index=i;
+    }
+    }
+    if(index!=-1){
+     cout<<"The marks "<<find<<" present at index "<<index;
+    }else{
+         cout<<"The marks "<<find<<"not present ";
+    }
+   
+    
     
      
     return 0;
