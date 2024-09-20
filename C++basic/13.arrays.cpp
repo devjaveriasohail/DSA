1b6c283502933aa48e36118224180675ec6b66d5
devjaveriasohail <javeriasohail996@gmail.com>
2024-09-20 10:42:13 +0500

    Day 8 arrays

--- C++basic/13.arrays.cpp
+++ C++basic/13.arrays.cpp
@@ -22,23 +22,40 @@ int main() {
     for(int i=0; i<6; i++){
         cout<<student[i]<<" ";
     }
+    cout<<endl;
 
    // find the smallest and largest value in array
+
    // input the values from user
    int value[10];
    int sizeValue=10; // fixing the size of value array and assigninig it to variable
+
     cout<<"Enter the values to find largest and smaller in them ";
     for(int i=0; i<sizeValue; i++){
         cin>>value[i];
     }
+
     // finding the largest and smallest value
-    int largest=value[0];
-     for(int i=0; i<5; i++){
-        if(value[i]>largest){
-           largest=value[i];
-        }
-        
+    int largest=INT_MIN;
+    int smallest=INT_MAX;
+     for(int i=0; i<sizeValue; i++){
+        // if(value[i]>largest){
+        //    largest=value[i];
+        // }
+        largest=max(value[i],largest);
+
+        // if(value[i]<smallest){
+        //     smallest=value[i];
+        // }
+
+       smallest= min(value[i],smallest);
     }
+    //printing the answer
      cout<<"\nThe largest value is "<<largest;
+     cout<<"\nThe smallest value is "<<smallest;
+     
+
+    
+     
     return 0;
 }
